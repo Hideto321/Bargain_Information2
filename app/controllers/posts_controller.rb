e2241f2d4e285170e946c6e5b8ba9fd17eab86d7
Hideto321 <gochanhidehide@gmail.com>
2020-10-19 16:04:32 +0900

    add_devise_controller_model_routes_view

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,38 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to posts_path
+  end
+  
+  def update
+
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:product, :price, :product_comment, :product_image)
+  end
+end
--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,23 +1,23 @@
 require 'test_helper'
 
-class ShopsControllerTest < ActionDispatch::IntegrationTest
+class PostsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get shops_index_url
+    get posts_index_url
     assert_response :success
   end
 
   test "should get show" do
-    get shops_show_url
+    get posts_show_url
     assert_response :success
   end
 
   test "should get edit" do
-    get shops_edit_url
+    get posts_edit_url
     assert_response :success
   end
 
   test "should get new" do
-    get shops_new_url
+    get posts_new_url
     assert_response :success
   end
 
