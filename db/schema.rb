1d9ea6a1734a4aae875c28bb35888acca4ad1cd6
Hideto321 <gochanhidehide@gmail.com>
2020-12-28 21:39:28 +0900

    add_column_sales-time_telephone

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_20_113731) do
+ActiveRecord::Schema.define(version: 2020_12_28_085549) do
 
   create_table "comments", force: :cascade do |t|
     t.text "comment"
@@ -38,6 +38,8 @@ ActiveRecord::Schema.define(version: 2020_10_20_113731) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "opening_hours"
+    t.integer "phone_number"
     t.index ["email"], name: "index_shops_on_email", unique: true
     t.index ["reset_password_token"], name: "index_shops_on_reset_password_token", unique: true
   end
